648c54f3931fdace1469d083bb7cdc94c144d329
Mutei Abuzahra <mutea200048@gmail.com>
2024-05-20 10:34:15 +0300

    furthur update 6

--- lib/screen/add_posts_screen.dart
+++ lib/screen/add_posts_screen.dart
@@ -1,3 +1,153 @@
+// import 'dart:io';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_database/firebase_database.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:firebase_storage/firebase_storage.dart';
+//
+// class AddPostScreen extends StatefulWidget {
+//   final Map<dynamic, dynamic>? post;
+//
+//   const AddPostScreen({super.key, this.post});
+//
+//   @override
+//   State<AddPostScreen> createState() => _AddPostScreenState();
+// }
+//
+// class _AddPostScreenState extends State<AddPostScreen> {
+//   final _formKey = GlobalKey<FormState>();
+//   final _titleController = TextEditingController();
+//   final _textController = TextEditingController();
+//   String _postId = '';
+//   File? _imageFile;
+//   final ImagePicker _picker = ImagePicker();
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (widget.post != null) {
+//       _postId = widget.post!['postId'];
+//       _titleController.text = widget.post!['NameEn'];
+//       _textController.text = widget.post!['Text'];
+//     }
+//   }
+//
+//   Future<void> _pickImage() async {
+//     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+//     if (pickedFile != null) {
+//       setState(() {
+//         _imageFile = File(pickedFile.path);
+//       });
+//     }
+//   }
+//
+//   Future<void> _savePost() async {
+//     if (_formKey.currentState!.validate()) {
+//       try {
+//         User? user = FirebaseAuth.instance.currentUser;
+//         if (user == null) {
+//           print('User not authenticated');
+//           return;
+//         }
+//         String userId = user.uid;
+//
+//         DatabaseReference userRef =
+//             FirebaseDatabase.instance.ref("App").child("User").child(userId);
+//         DatabaseEvent userEvent = await userRef.once();
+//         Map<dynamic, dynamic>? userData =
+//             userEvent.snapshot.value as Map<dynamic, dynamic>?;
+//
+//         if (userData == null) {
+//           print('User data not found for user ID: $userId');
+//           return;
+//         }
+//
+//         String userName = "${userData['FirstName']} ${userData['LastName']}";
+//         String profileImageUrl = userData['ProfileImageUrl'] ?? '';
+//
+//         DatabaseReference postsRef =
+//             FirebaseDatabase.instance.ref("App").child("AllPosts");
+//
+//         if (_postId.isEmpty) {
+//           _postId = postsRef.push().key!;
+//         }
+//
+//         String? imageUrl;
+//         if (_imageFile != null) {
+//           UploadTask uploadTask = FirebaseStorage.instance
+//               .ref()
+//               .child('post_images')
+//               .child('$_postId.jpg')
+//               .putFile(_imageFile!);
+//           TaskSnapshot snapshot = await uploadTask;
+//           imageUrl = await snapshot.ref.getDownloadURL();
+//         }
+//
+//         await postsRef.child(_postId).set({
+//           'NameEn': _titleController.text,
+//           'Text': _textController.text,
+//           'Date': DateTime.now().millisecondsSinceEpoch,
+//           'UserName': userName,
+//           'ProfileImageUrl': profileImageUrl,
+//           'userId': userId,
+//           'ImageUrl': imageUrl ?? '',
+//         });
+//
+//         print('Post saved successfully');
+//         Navigator.pop(context);
+//       } catch (e) {
+//         print('Error saving post: $e');
+//       }
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.post == null ? "Add Post" : "Edit Post"),
+//       ),
+//       body: SingleChildScrollView(
+//         child: Form(
+//           key: _formKey,
+//           child: Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               children: [
+//                 TextFormField(
+//                   controller: _titleController,
+//                   decoration: InputDecoration(labelText: "Title"),
+//                   validator: (value) {
+//                     if (value == null || value.isEmpty) {
+//                       return 'Please enter a title';
+//                     }
+//                     return null;
+//                   },
+//                 ),
+//                 TextFormField(
+//                   controller: _textController,
+//                   decoration: InputDecoration(labelText: "Text"),
+//                 ),
+//                 SizedBox(height: 20),
+//                 _imageFile == null
+//                     ? Text("No image selected.")
+//                     : Image.file(_imageFile!),
+//                 ElevatedButton(
+//                   onPressed: _pickImage,
+//                   child: Text("Pick Image"),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: _savePost,
+//                   child: Text(widget.post == null ? "Save" : "Update"),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -19,7 +169,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
   final _titleController = TextEditingController();
   final _textController = TextEditingController();
   String _postId = '';
-  File? _imageFile;
+  List<File> _imageFiles = [];
   final ImagePicker _picker = ImagePicker();
 
   @override
@@ -32,11 +182,12 @@ class _AddPostScreenState extends State<AddPostScreen> {
     }
   }
 
-  Future<void> _pickImage() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
+  Future<void> _pickImages() async {
+    final pickedFiles = await _picker.pickMultiImage();
+    if (pickedFiles != null) {
       setState(() {
-        _imageFile = File(pickedFile.path);
+        _imageFiles =
+            pickedFiles.map((pickedFile) => File(pickedFile.path)).toList();
       });
     }
   }
@@ -72,15 +223,16 @@ class _AddPostScreenState extends State<AddPostScreen> {
           _postId = postsRef.push().key!;
         }
 
-        String? imageUrl;
-        if (_imageFile != null) {
+        List<String> imageUrls = [];
+        for (File imageFile in _imageFiles) {
           UploadTask uploadTask = FirebaseStorage.instance
               .ref()
               .child('post_images')
-              .child('$_postId.jpg')
-              .putFile(_imageFile!);
+              .child('$_postId${imageFile.path.split('/').last}')
+              .putFile(imageFile);
           TaskSnapshot snapshot = await uploadTask;
-          imageUrl = await snapshot.ref.getDownloadURL();
+          String imageUrl = await snapshot.ref.getDownloadURL();
+          imageUrls.add(imageUrl);
         }
 
         await postsRef.child(_postId).set({
@@ -90,7 +242,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
           'UserName': userName,
           'ProfileImageUrl': profileImageUrl,
           'userId': userId,
-          'ImageUrl': imageUrl ?? '',
+          'ImageUrls': imageUrls,
         });
 
         print('Post saved successfully');
@@ -129,12 +281,21 @@ class _AddPostScreenState extends State<AddPostScreen> {
                   decoration: InputDecoration(labelText: "Text"),
                 ),
                 SizedBox(height: 20),
-                _imageFile == null
-                    ? Text("No image selected.")
-                    : Image.file(_imageFile!),
+                _imageFiles.isEmpty
+                    ? Text("No images selected.")
+                    : Container(
+                        height: 100,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: _imageFiles.length,
+                          itemBuilder: (context, index) {
+                            return Image.file(_imageFiles[index]);
+                          },
+                        ),
+                      ),
                 ElevatedButton(
-                  onPressed: _pickImage,
-                  child: Text("Pick Image"),
+                  onPressed: _pickImages,
+                  child: Text("Pick Images"),
                 ),
                 ElevatedButton(
                   onPressed: _savePost,
--- lib/widgets/reused_all_posts_cards.dart
+++ lib/widgets/reused_all_posts_cards.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ReusedAllPostsCards extends StatelessWidget {
+class ReusedAllPostsCards extends StatefulWidget {
   final Map post;
   final String? currentUserId;
   final VoidCallback onEdit;
@@ -13,7 +13,33 @@ class ReusedAllPostsCards extends StatelessWidget {
   });
 
   @override
+  _ReusedAllPostsCardsState createState() => _ReusedAllPostsCardsState();
+}
+
+class _ReusedAllPostsCardsState extends State<ReusedAllPostsCards> {
+  late PageController _pageController;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _pageController.addListener(() {
+      setState(() {
+        _currentPage = _pageController.page?.round() ?? 0;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    List imageUrls = widget.post['ImageUrls'] ?? [];
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -22,44 +48,77 @@ class ReusedAllPostsCards extends StatelessWidget {
           children: [
             ListTile(
               leading: CircleAvatar(
-                backgroundImage: post['ProfileImageUrl'] != null
-                    ? NetworkImage(post['ProfileImageUrl'])
+                backgroundImage: widget.post['ProfileImageUrl'] != null
+                    ? NetworkImage(widget.post['ProfileImageUrl'])
                     : null,
-                child:
-                    post['ProfileImageUrl'] == null ? Icon(Icons.person) : null,
+                child: widget.post['ProfileImageUrl'] == null
+                    ? Icon(Icons.person)
+                    : null,
               ),
-              title: Text(post['UserName'] ?? 'Unknown User'),
+              title: Text(widget.post['UserName'] ?? 'Unknown User'),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(post['Text'] ?? 'No Text Available'),
-                  Text(post['NameEn'] ?? 'Unknown Name'),
+                  Text(widget.post['Text'] ?? 'No Text Available'),
+                  Text(widget.post['NameEn'] ?? 'Unknown Name'),
                 ],
               ),
-              trailing: Text(post['RelativeDate'] ?? 'Unknown Date'),
+              trailing: Text(widget.post['RelativeDate'] ?? 'Unknown Date'),
             ),
-            if (post['ImageUrl'] != null && post['ImageUrl'].isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.network(
-                  post['ImageUrl'],
-                  loadingBuilder: (BuildContext context, Widget child,
-                      ImageChunkEvent? loadingProgress) {
-                    if (loadingProgress == null) return child;
-                    return Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                (loadingProgress.expectedTotalBytes ?? 1)
-                            : null,
+            if (imageUrls.isNotEmpty)
+              Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    height: 300, // Adjust the height as needed
+                    child: PageView.builder(
+                      controller: _pageController,
+                      itemCount: imageUrls.length,
+                      itemBuilder: (context, index) {
+                        return Image.network(
+                          imageUrls[index],
+                          fit: BoxFit.cover,
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) return child;
+                            return Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes !=
+                                        null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        (loadingProgress.expectedTotalBytes ??
+                                            1)
+                                    : null,
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 8,
+                    right: 16,
+                    child: Container(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 8,
                       ),
-                    );
-                  },
-                ),
+                      decoration: BoxDecoration(
+                        color: Colors.black54,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Text(
+                        '${_currentPage + 1}/${imageUrls.length}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            if (post['userId'] == currentUserId)
+            if (widget.post['userId'] == widget.currentUserId)
               TextButton(
-                onPressed: onEdit,
+                onPressed: widget.onEdit,
                 child: Text("Edit Post"),
               ),
           ],
