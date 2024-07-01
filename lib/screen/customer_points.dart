caa992eef79723688e6b98eaa79ab58ad5ca3722
Mutei Abuzahra <mutea200048@gmail.com>
2024-07-01 16:42:14 +0300

    further update 86

--- lib/screen/customer_points.dart
+++ lib/screen/customer_points.dart
@@ -1,6 +1,8 @@
 import 'package:diamond_booking/constants/colors.dart';
 import 'package:diamond_booking/constants/styles.dart';
 import 'package:diamond_booking/localization/language_constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class CustomerPoints extends StatefulWidget {
@@ -11,6 +13,39 @@ class CustomerPoints extends StatefulWidget {
 }
 
 class _CustomerPointsState extends State<CustomerPoints> {
+  final DatabaseReference _databaseReference =
+      FirebaseDatabase.instance.ref().child('App/ProviderFeedbackToCustomer');
+  double _totalPoints = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchCustomerPoints();
+  }
+
+  void _fetchCustomerPoints() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final userId = user.uid;
+      final ratingsRef = _databaseReference.child(userId);
+
+      ratingsRef.onValue.listen((event) {
+        double totalPoints = 0.0;
+        if (event.snapshot.value != null) {
+          final ratingsData =
+              Map<String, dynamic>.from(event.snapshot.value as Map);
+          ratingsData.forEach((key, value) {
+            final rating = (value['rating'] ?? 0).toDouble();
+            totalPoints += rating;
+          });
+        }
+        setState(() {
+          _totalPoints = totalPoints;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +58,12 @@ class _CustomerPointsState extends State<CustomerPoints> {
             "My Points",
           ),
           style: const TextStyle(color: kPrimaryColor),
+        ),
+      ),
+      body: Center(
+        child: Text(
+          '${getTranslated(context, "Total Points")}: $_totalPoints',
+          style: const TextStyle(fontSize: 24),
         ),
       ),
     );
